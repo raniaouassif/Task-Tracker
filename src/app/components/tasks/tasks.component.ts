21e08ae140a57f28ad316d52e0e2393abc50a427
Rania Ouassif <rania.ouassif@mail.mcgill.ca>
2023-08-16 17:06:58 -0400

    Observable getTask

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-task';
+import { TaskService } from 'src/app/services/task.service';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -8,11 +9,13 @@ import { TASKS } from 'src/app/mock-task';
 })
 export class TasksComponent implements OnInit{
 
-  tasks: Task[] = TASKS;
-  
-  constructor() {}
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
   ngOnInit(): void {
-    
+    this.taskService.getTasks().subscribe((tasks) =>
+      this.tasks = tasks);
   }
 
 }
