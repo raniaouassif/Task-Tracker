58c19e8ff685e2700497d7a8fcf4e9920d6153f1
Rania Ouassif <rania.ouassif@mail.mcgill.ca>
2023-08-16 13:38:55 -0400

    comments to appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //HTML tag used to embed the component
+  templateUrl: './app.component.html', //HTML file we are using
+  styleUrls: ['./app.component.css'] //can have more than 1
 })
-export class AppComponent {
-  title = 'angular-crash';
+
+export class AppComponent { //Put any properties of the component
 }
