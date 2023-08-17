51efd3abca3b2f0918c45e3c25e4bc03c9af5596
Rania Ouassif <rania.ouassif@mail.mcgill.ca>
2023-08-17 10:41:55 -0400

    Router for main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule} from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
 
+const appRoutes: Routes = [
+  {path: '', component : TasksComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,13 +23,15 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing : true})
   ],
   providers: [],
   bootstrap: [AppComponent]
