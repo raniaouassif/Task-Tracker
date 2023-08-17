de09c688dc11390058cf7b95237905bb74a8e53d
Rania Ouassif <rania.ouassif@mail.mcgill.ca>
2023-08-17 11:18:19 -0400

    Add shows only on main page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,9 +14,10 @@ export class HeaderComponent implements OnInit{
   subscription: Subscription = new Subscription;
 
   
-  constructor(private uiService : UiService) {
+  constructor(private uiService : UiService, private router:Router) {
     this.subscription = this.uiService.onToggle().subscribe((value) =>
       this.showAddTask = value);
+    
   }
 
   ngOnInit(): void { 
@@ -24,6 +26,10 @@ export class HeaderComponent implements OnInit{
 
   toggleAddTask() {
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 
 }
